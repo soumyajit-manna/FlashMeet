a6725a472df288aba37b3a3864d57fd0a203a3f2
soumyajit-manna <mannasoumyajit47@gmail.com>
2024-08-17 01:59:00 +0530

    DEPLOYING FRONTEND

---
+++ frontend/src/environment.js
@@ -0,0 +1,8 @@
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://flashmeet-backend-v6hg.onrender.com" :
+
+    "http://localhost:8000"
+
+
+export default server;
--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -14,7 +14,7 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 var connections = {};
 
