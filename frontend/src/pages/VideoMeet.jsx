225a1f75442e27b82073ad08f3cf2d22921b7629
soumyajit-manna <mannasoumyajit47@gmail.com>
2024-08-17 02:55:12 +0530

    DEPLOYING FRONTEND

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -12,6 +12,7 @@ import MicOffIcon from "@mui/icons-material/MicOff"
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
+import server from '../environment';
 
 
 const server_url = server;
